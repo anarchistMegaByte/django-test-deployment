c7ba457507ac00fa8c8b98ffcc2a06828e76274d
anarchistMegaByte <manavbharambe93@gmail.com>
2018-07-06 07:31:18 +0530

    Congif settings.py with herokudb

--- DjangoSampleProject/settings.py
+++ DjangoSampleProject/settings.py
@@ -15,6 +15,11 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500, require_ssl=True)
+
+DATABASES['default'].update(db_from_env)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
