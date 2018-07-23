1471607e044760880fc3376646a3c9bf29dcb7e9
anarchistMegaByte <manavbharambe93@gmail.com>
2018-07-23 19:33:25 +0530

    saving req into db

--- dmWebSite/views.py
+++ dmWebSite/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from dmWebSite.models import dmWebsiteUser
 import requests  
 import json
 
@@ -9,7 +10,8 @@ import json
 def saveUserInfo(request):
     if request.method == 'POST':
         received_json_data=json.loads(request.body)
-        answer = received_json_data['user_name']
+        u1 = dmWebsiteUser(user_name=received_json_data['user_name'], user_email=received_json_data['user_email'], user_phone=received_json_data['user_phone'])
+        u1.save()
         return HttpResponse(answer)
     else:
         return HttpResponse("Hello World")
