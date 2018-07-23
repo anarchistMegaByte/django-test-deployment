35108f0d32ac072ee7508bd974702f379b2d6e0c
anarchistMegaByte <manavbharambe93@gmail.com>
2018-07-23 17:35:41 +0530

    Made table visible in admin interface plus object id replaced by user_name

--- dmWebSite/admin.py
+++ dmWebSite/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import dmWebsiteUser
 
 # Register your models here.
+admin.site.register(dmWebsiteUser)
--- dmWebSite/models.py
+++ dmWebSite/models.py
@@ -5,3 +5,6 @@ class dmWebsiteUser(models.Model):
     user_name = models.CharField(max_length=50)
     user_email = models.CharField(max_length=50)
     user_phone = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.user_name
