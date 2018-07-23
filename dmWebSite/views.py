154d53949982c2adf178b9ee94601bc3a021d8c4
anarchistMegaByte <manavbharambe93@gmail.com>
2018-07-23 19:00:29 +0530

    Accept Requests to in json format

--- dmWebSite/views.py
+++ dmWebSite/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests  
+import json
 
 # Create your views here.
 def saveUserInfo(request):
-    return HttpResponse("Hello world")
-    #if request.method == 'POST':
+    if request.method == 'POST':
+        received_json_data=json.loads(request.body)
+        return HttpResponse(received_json_data)
 
