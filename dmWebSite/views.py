c5001328107597668c88266ca5c1a9bc00e2e817
anarchistMegaByte <manavbharambe93@gmail.com>
2018-07-23 19:08:45 +0530

    Added csrf token exmpt tag to function view

--- dmWebSite/views.py
+++ dmWebSite/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template.defaulttags import csrf_token
 import requests  
 import json
 
 # Create your views here.
+@csrf_excempt
 def saveUserInfo(request):
     if request.method == 'POST':
         received_json_data=json.loads(request.body)
