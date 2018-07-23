951e596afac44e061e7c4d72d1a093912477b10e
anarchistMegaByte <manavbharambe93@gmail.com>
2018-07-23 17:44:20 +0530

    Adding columns to tables to admin interface

--- dmWebSite/admin.py
+++ dmWebSite/admin.py
@@ -2,4 +2,6 @@ from django.contrib import admin
 from .models import dmWebsiteUser
 
 # Register your models here.
-admin.site.register(dmWebsiteUser)
+class dmWebsiteUserAdmin(admin.ModelAdmin):
+    list_display = ('user_name', 'user_email', 'user_phone')
+admin.site.register(dmWebsiteUser, dmWebsiteUserAdmin)
