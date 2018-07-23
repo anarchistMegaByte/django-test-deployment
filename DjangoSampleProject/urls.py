eb0079d9c994331ad72f363d21999fd63348c6b7
anarchistMegaByte <manavbharambe93@gmail.com>
2018-07-23 18:52:12 +0530

    Created first Url

--- DjangoSampleProject/urls.py
+++ DjangoSampleProject/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from dmWebSite.views import saveUserInfo
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^user_info/', saveUserInfo)
 ]
