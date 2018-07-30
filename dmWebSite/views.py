7c414e45dff7f8bc32fdefeb965690563b941cf1
anarchistMegaByte <manavbharambe93@gmail.com>
2018-07-30 13:34:58 +0530

    Avooided repeated entries in table

--- dmWebSite/views.py
+++ dmWebSite/views.py
@@ -11,9 +11,17 @@ import json
 def saveUserInfo(request):
     if request.method == 'POST':
         received_json_data=json.loads(request.body)
-        u1 = dmWebsiteUser(user_name=received_json_data['user_name'], user_email=received_json_data['user_email'], user_phone=received_json_data['user_phone'])
+        u1, created = dmWebsiteUserOnlyEmails.objects.get_or_create(
+                            user_name=received_json_data['user_name'], 
+                            user_email=received_json_data['user_email'], 
+                            user_phone=received_json_data['user_phone']
+                        )
+        # u1 = dmWebsiteUser(user_name=received_json_data['user_name'], user_email=received_json_data['user_email'], user_phone=received_json_data['user_phone'])
         u1.save()
-        return HttpResponse("Success!!")
+        if created:
+            return HttpResponse("Success!! Created : " + str(created))
+        else:
+            return HttpResponse("Success!! Entry already present")
     else:
         return HttpResponse("Not a POST request.")
 
